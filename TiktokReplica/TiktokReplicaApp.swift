a1e936cc38ae836d7626ca3000e0920f8ca2f75a
biozalp <biozalp@biozalp.com>
2024-02-03 16:44:51 +0300

    Initial Commit

---
+++ TiktokReplica/TiktokReplicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TiktokReplicaApp.swift
+//  TiktokReplica
+//
+//  Created by Berk Ilgar Özalp on 3.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TiktokReplicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
