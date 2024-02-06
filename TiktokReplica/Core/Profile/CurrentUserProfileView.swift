394c8b3c1e2ffa02095d75b5eba8733099e7c1d8
biozalp <biozalp@biozalp.com>
2024-02-06 17:22:27 +0300

    up to header view

---
+++ TiktokReplica/Core/Profile/CurrentUserProfileView.swift
@@ -0,0 +1,27 @@
+//
+//  CurrentUserProfileView.swift
+//  TiktokReplica
+//
+//  Created by Berk Ilgar Özalp on 6.02.2024.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                VStack(spacing: 2){
+                    //profile header
+                    
+                    
+                    // post grid view
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
